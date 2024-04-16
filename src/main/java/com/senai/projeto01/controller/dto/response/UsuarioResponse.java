c3881b6da3ca2dba76d74f99eacd17a57ee83f29
panha <pamela_vs@estudante.sc.senai.br>
2024-04-16 20:13:01 -0300

    :sparkles: feat: atualiza dto de UsuarioResponse

--- src/main/java/com/senai/projeto01/controller/dto/response/UsuarioResponse.java
+++ src/main/java/com/senai/projeto01/controller/dto/response/UsuarioResponse.java
@@ -1,7 +1,5 @@
 package com.senai.projeto01.controller.dto.response;
 
-import com.senai.projeto01.datasource.entity.PapelEntity;
-
 public record UsuarioResponse(
         Long id,
         String nomeUsuario,
